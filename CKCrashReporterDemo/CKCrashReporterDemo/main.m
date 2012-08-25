9c88d5187d075d1dc2321806c58fb0accf46f487
Giulio Petek <gi-lo@touch-mania.com>
2012-08-24 23:33:29 -0700

    Adds copyright information

--- CKCrashReporterDemo/CKCrashReporterDemo/main.m
+++ CKCrashReporterDemo/CKCrashReporterDemo/main.m
@@ -1,6 +1,7 @@
 /* ----------------------------------------------------------------------
  main.m
- 
+ Copyright 2012 Giulio Petek. All rights reserved.
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
